500feec4ab797f8031598cfa1f89ba7062fa65ed
BuildTools <unconfigured@null.spigotmc.org>
2018-01-10 22:04:20 +0200

    mäng

---
+++ Assets/pudel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pudel : MonoBehaviour {
+
+    public GameObject player;
+    //private Collider c;
+    public GameObject obj;
+
+	// Use this for initialization
+	void Start () {
+       //Collider c = gameObject.GetComponent<BoxCollider>;
+	}
+
+    private void OnTriggerEnter(Collider player)
+    {
+        obj.SetActive(false);
+      
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
